6612a058f66df20d8f4668e8a95c8d0ccb439e18
benoit brandt <benoit.07@hotmail.com>
2024-11-29 22:11:04 +0100

    Replace logic - first argument could be '-i' if we want to create the makefile wth source in src folder and header in inc folder as well we keep the same logic for all generat*.sh
    Add a logic to replace two placeholder in the makefile
    placeholder_name should be the first argument of the command or th second with '-i' option
    	format: <name>
    placeholder_src_file should be the second arg of the the cmd or the third with '-i' option
    	format: <"source1 source2 source3 ...">

--- generateMakefileCpp.sh
+++ generateMakefileCpp.sh
@@ -2,11 +2,8 @@
 
 # Check if number of argument is one
 
-if [ $# -lt 1 ]; then
-  echo 1>&2 "$0: not enough arguments - Please insert one option root or folder"
-  exit 2
-elif [ $# -gt 1 ]; then
-  echo 1>&2 "$0: too many arguments - Please insert one option root or folder"
+if [ $# -gt 3 ]; then
+  echo 1>&2 "$0: too many arguments -  (<-i>) <program_name> <string of src file>"
   exit 2
 fi
 
@@ -17,18 +14,33 @@ if test -f "Makefile"; then
 fi
 
 makefile=Makefile
-makefileInRoot=~/generate_cpp_class/src_inc_in_root.mk
-makefileInFolder=~/generate_cpp_class/src_inc_in_folder.mk
+sourceInRoot=~/generate_cpp_files/src_inc_in_root.mk
+sourceInFolder=~/generate_cpp_files/src_inc_in_folder.mk
 
-if [[ $1 = "root" ]]
-	then
-		cp $makefileInRoot $makefile
-        echo Makefile is create
-elif [[ $1 = "folder" ]]
-	then
-		cp $makefileInFolder $makefile
-        echo Makefile is create
-	else
-		echo Error: Wrong argument
-		exit 0
+if [[ $1 = "-i" ]]; then
+	cp $sourceInFolder $makefile
+	if [ $# -eq 3 ]; then
+		programName=$2
+		sourceFiles=$3
+	fi
+else
+	cp $sourceInRoot $makefile
+	if [ $# -eq 2 ]; then
+		programName=$1
+		sourceFiles=$2
+	fi
+fi
+
+echo Makefile is create
+
+# If argument is equal to 3 replace the corrresponding value in the makefile
+if [ $# -eq 3 ] || ([ $# -eq 2 ] && [ "$1" != "-i" ]); then
+	placeholderName="placeholder_name"
+	placeholderSourceFile="placeholder_src_file"
+	# replace "placeholder_name" by the name of the programme
+	sed -i "s/$placeholderName/$programName/" $makefile
+	echo "$placeholderName in your Makefile is replace by $programName"
+	# replace "placholder_source_file" word by the source file string
+	sed -i "s/$placeholderSourceFile/$sourceFiles/" $makefile
+	echo "$placeholderSourceFile in your Makefile is replace by '$sourceFiles'"
 fi
