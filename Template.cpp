ae1efd91f99304397e8d1aace632e8ac58a9fdd9
Benoilte <benoit.07@hotmail.com>
2024-12-19 17:16:06 +0100

    Update the generator of CPP class to be conform to the canonical class form And add some usefull utility
    Add copy constructor
    
    Add Copy assignment operator
    
    Add Parameterized Constructor
    
    Add private _foo variable
    
    An overload of the insertion («) operator

--- Template.cpp
+++ Template.cpp
@@ -5,7 +5,39 @@ Template::Template()
     /*Constructor*/
 }
 
+Template::Template(const Template &src)
+{
+	*this = src;
+
+	return ;
+}
+
+Template::Template(int const n) : _foo(n)
+{
+	
+}
+
 Template::~Template()
 {
-    /*Destructor*/
+	/*Destructor*/
+}
+
+Template& Template::operator=(const Template &rhs)
+{
+	if (this != &rhs)
+		this->_foo = rhs.getFoo();
+	
+	return *this;
+
+}
+
+int Template::getFoo(void) const
+{
+	return _foo;
+}
+
+std::ostream& operator<<(std::ostream &out, Template const &rhs)
+{
+	out << rhs.getFoo();
+	return out; 
 }
--- Template.hpp
+++ Template.hpp
@@ -1,16 +1,27 @@
 #ifndef TEMPLATE_HPP
 #define TEMPLATE_HPP
 
+# include <iostream>
+
 class Template
 {
 	public:
 
 		Template();
+		Template(int const n);
+		Template(const Template &src);
 		~Template();
+
+		Template	&operator=(const Template &rhs);
+
+		int		getFoo(void) const;
+
 
 	private:
 	
-		/* data */
+		int	_foo;
 };
+
+std::ostream	&operator<<(std::ostream &out, Template const &rhs);
 
 #endif
